1400c8b7739947b473ecd22343aa226c7e321312
Santiago Parra <duvansantiagoparrao14@gmail.com>
2024-11-06 15:45:31 -0500

    commit v2

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const path = require('path')
+const colors = require('colors')
 const app = express();
+// ejs
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// rutas
 
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "Login.html"));
+    res.render("login");
 });
-
 app.get('/user', function (req, res) {
-    res.sendFile(path.join(__dirname, "bienvenido.html"));
+    res.render("bienvenido");
 });
 app.get('/datos', function (req, res) {
-    res.sendFile(path.join(__dirname, "datos.html"));
+    res.render("datos");
 });
-
-
  const  puerto = 3000
-console.log(`Servidor corriendo en el puerto ${puerto}`);
+console.log(`Servidor corriendo en el puerto ${puerto}`.green);
 app.listen(puerto);
