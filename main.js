de3851f731d13555f8d1e3b7706520ec23618c22
Santiago Parra <duvansantiagoparrao14@gmail.com>
2024-11-07 07:20:36 -0500

    vercion 3

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const colors = require('colors')
 const app = express();
+const mysql = require('mysql2');
+// Configuración de la conexión a la base de datos
+var conexioncita = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'santiago',
+    database: 'jefe'
+});
+
+conexioncita.connect(error => {
+    if (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        process.exit(1);
+    }
+    console.log('Conectado a la base de datos'.green);
+});
 // ejs
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -13,11 +29,44 @@ app.get('/', function (req, res) {
     res.render("login");
 });
 app.get('/user', function (req, res) {
-    res.render("bienvenido");
+    res.render("bienvenido" ,{usuario});
 });
 app.get('/datos', function (req, res) {
-    res.render("datos");
+    res.render("datos",{datosUsuario, fecha});
 });
+let datosUsuario
+let fecha
+// validar usuario y contraseña
+
+
+app.post('/', function (req, res) {
+    const datos = req.body;
+    console.log(datos);
+
+    let nombre = datos.nombre;
+    let cedula = datos.contraseña;
+    let validar = `SELECT * FROM usuarios WHERE nombre1 = '${nombre}' AND identificacion = '${cedula}'`;
+
+    conexioncita.query(validar, function (error, results, fields) {
+        if (error) {
+            console.error('Error al validar usuario:', error);
+            res.status(500).send('Error al iniciar sesión');
+            return;
+        }
+
+        if (results.length === 1) {
+            console.log("Bienvenido".blue);
+            datosUsuario = results[0];
+            res.render('bienvenido', { usuario: datosUsuario });
+        } else {
+            console.log("Contraseña incorrecta".red);
+            res.redirect("/");
+        }
+    });
+});
+
+console.log(datosUsuario)
+//desplegar el servidr
  const  puerto = 3000
 console.log(`Servidor corriendo en el puerto ${puerto}`.green);
 app.listen(puerto);
